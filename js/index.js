667818461d5b216fe226a61a06856e5499dea3d4
Panda1000000 <201931002tt@pcnet.hue.ac.jp>
2021-04-20 15:50:47 +0000

    Commit !

---
+++ js/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function hello(params) {
+    return `Hello ${params}`;
+}
+console.log(hello("Panda-Man"));
+console.log(hello("Panda-woman"));
---
+++ ts/index.ts
@@ -0,0 +1,6 @@
+function hello(params:string) {
+    return `Hello ${params}`;
+}
+
+console.log(hello("Panda-Man"));
+console.log(hello("Panda-woman"));
